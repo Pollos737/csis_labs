471c28ba7402f90377c29d4ded97227144175a66
Wilmer Guerra <Wilmer Guerra>
2016-03-06 21:38:24 -0800

    next

--- csis_labs/Source.c
+++ csis_labs/Source.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-	int i = 1;
-	while (i <= 64) {
-		printf("%d", i);
-		i *= 2;
-	}
+	int i = 1234;
+	do {
+		printf("%d\n", i);
+		i /= 10;
+	} while (i > 0);
 	return 0;
 }
